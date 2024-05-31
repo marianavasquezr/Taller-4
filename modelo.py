f07f0134093c32b0ee14917d9b52690c3c351c9d
estefania <estefania.loaizas@udea.edu.co>
2024-05-30 20:43:33 -0500

    v4: cre los metodos de guardar la info, de agregar paciente, borrar paciente

--- modelo.py
+++ modelo.py
@@ -12,3 +12,26 @@ class PacModel:
                 self.patients = json.load(file)
         else:
             self.patients = []
+
+    def save_data(self):
+        with open(self.data_file, 'w') as file:
+            json.dump(self.patients, file, indent=4)
+
+    def add_patient(self, patient:dict): 
+        if not any(p['id'] == patient['id'] for p in self.patients):
+            self.patients.append(patient)
+            self.save_data()
+            return True
+        return False
+
+    def delete_patient(self, patient_id:str):
+        initLen = len(self.pets)
+        self.patients = [p for p in self.patient if p['id'] != patient_id]
+        self.save_data()
+        if initLen == len(self.patients):
+            return 0
+        else:
+            return 1
+
+    def search_patients(self, initName:str):
+        pass 
